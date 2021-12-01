dfcb8b0c9c5fac5a28ce1095e85f3aeb31804000
Jeremy Martin <jeremy.martin@smart-chain.fr>
2021-12-01 17:28:39 +0100

    fix claimall : feed 0 only on elapsed weeks

--- src/contract/test/test_farm.py
+++ src/contract/test/test_farm.py
@@ -353,56 +353,23 @@ class FarmsContractTest(TestCase):
     ######################
 
     def test_claimall_basic_should_work(self):
-
         init_storage = deepcopy(initial_storage)
         init_storage["total_reward"] = 20_000_000
-        init_storage["reward_at_week"] = {
-            1: 6555697,
-            2: 4916773,
-            3: 3687580,
-            4: 2765685,
-            5: 2074263
-        }
+        init_storage["reward_at_week"] = [6555697, 4916773, 3687580, 2765685, 2074263]
         init_storage["creation_time"] = 0
-        init_storage["user_stakes"] = {
-            alice: 500
-        }
-        init_storage["user_points"] = {
-            alice: {
-                1: int(500 * sec_week/2),
-                2: 500 * sec_week,
-                3: 500 * sec_week,
-                4: 500 * sec_week,
-                5: 500 * sec_week
-            }
-        }
-        init_storage["farm_points"] = {
-            1: int(500 * sec_week / 2),
-            2: 500 * sec_week,
-            3: 500 * sec_week,
-            4: 500 * sec_week,
-            5: 500 * sec_week
-        }
-
+        init_storage["user_stakes"][alice] = 500
+        init_storage["user_points"][alice] = [int(500 * sec_week/2), 500 * sec_week, 500 * sec_week, 500 * sec_week, 500 * sec_week]
+        init_storage["farm_points"] = [int(500 * sec_week/2), 500 * sec_week, 500 * sec_week, 500 * sec_week, 500 * sec_week]
         res = self.farms.claim_all().interpret(storage=init_storage, sender=alice, now=int(sec_week + sec_week/2))
-
-        self.assertEqual(admin, res.storage["admin"])
-        transfer_txs = res.operations
-
-        self.assertEqual(1, len(transfer_txs))
-        self.assertEqual('transaction', transfer_txs[0]["kind"])
-        transfer_tx_params = transfer_txs[0]["parameters"]["value"]['args']
-        self.assertEqual(initial_storage["reserve_address"], transfer_tx_params[0]['string'])
-        self.assertEqual(alice, transfer_tx_params[1]['string'])
-        self.assertEqual(str(init_storage["reward_at_week"][1]), transfer_tx_params[2]['int'])
-
-        alice_points = res.storage["user_points"][alice]
-        self.assertEqual(alice_points[1], 0)
-        self.assertEqual(alice_points[2], 500 * sec_week)
-        self.assertEqual(alice_points[3], 500 * sec_week)
-        self.assertEqual(alice_points[4], 500 * sec_week)
-        self.assertEqual(alice_points[5], 500 * sec_week)
-
+        self.assertEqual(res.storage["admin"], admin)
+        transfer_tx_params = res.operations[0]["parameters"]["value"]['args']
+        self.assertEqual(transfer_tx_params[0]['string'], initial_storage["reward_reserve_address"])
+        self.assertEqual(transfer_tx_params[1]['string'], alice)
+        self.assertEqual(int(transfer_tx_params[2]['int']), init_storage["reward_at_week"][0])
+        expected_userpoint_alice = [0, 500 * sec_week, 500 * sec_week, 500 * sec_week, 500 * sec_week]
+        self.assertEqual(res.storage["user_points"][alice], expected_userpoint_alice)
+        self.assertEqual(res.storage["user_stakes"][alice], 0)
+        self.assertEqual(res.storage["farm_points"], init_storage["farm_points"] )
 
     def test_claimall_with_0_points_should_fail(self):
 
